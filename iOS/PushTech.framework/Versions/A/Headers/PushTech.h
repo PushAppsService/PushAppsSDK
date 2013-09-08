bd784f872a85ce0b56616103e7ce9b45b17e82c9
Asaf Ron <asaf@groboot.com>
2013-09-08 19:44:41 +0300

    First commit

---
+++ iOS/PushTech.framework/Versions/A/Headers/PushTech.h
@@ -0,0 +1,10 @@
+//
+//  PushTech.h
+//  PushTech
+//
+//  Created by Asaf Ron on 6/27/13.
+//  Copyright (c) 2013 Groboot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <PushTech/PushTechManager.h>
