774a9994ee8ff93e52cd1000bafd89d73fabee52
Asaf Ron <asaf@groboot.com>
2013-09-30 22:00:08 +0300

    no message

--- iOS/PushApps.framework/Versions/A/Headers/PushApps.h
+++ iOS/PushApps.framework/Versions/A/Headers/PushApps.h
@@ -1,10 +1,10 @@
 //
-//  PushTech.h
-//  PushTech
+//  PushApps.h
+//  PushApps
 //
 //  Created by Asaf Ron on 6/27/13.
 //  Copyright (c) 2013 Groboot. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
-#import <PushTech/PushTechManager.h>
+#import <PushApps/PushAppsManager.h>
