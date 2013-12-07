8924171439b1bedf9bd1eb4a4ed1a5c46ea550c6
PushApps Service <info@pushapps.mobi>
2013-12-08 00:49:06 +0200

    Android Demo and SDK
    - constants for notification keys
    - more examples in DemoApplication.java

--- Android/Demo/src/com/example/pushappsdemo/DemoApplication.java
+++ Android/Demo/src/com/example/pushappsdemo/DemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.pushappsdemo;
 
+import com.groboot.pushapps.PushAppsMessageInterface;
 import com.groboot.pushapps.PushAppsRegistrationInterface;
 import com.groboot.pushapps.PushManager;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 public class DemoApplication extends Application {
@@ -16,6 +18,25 @@ public class DemoApplication extends Application {
 	public void onCreate() {
 		super.onCreate();
 		PushManager.init(getApplicationContext(), GOOGLE_API_PROJECT_ID, PUSHAPPS_APP_TOKEN);
+		
+	  //optional, default is the app icon
+		PushManager.getInstance(getApplicationContext()).setNotificationIcon(R.drawable.notification_icon); 
+		
+		//optional , if not set then the launcher activity will called after clicking the notification
+		//PushManager.getInstance(getApplicationContext()).setIntentNameToLaunch("com.groboo.pushapps.SomeActivity");
+		
+		/*
+		 * optional, if you want to take control over the event upon receiving
+		 * notification
+		 * PushManager.getInstance(getApplicationContext()).registerForMessagesEvents
+		 * (new PushAppsMessageInterface() {
+		 * 
+		 * @Override public void onMessage(Context arg0, Intent arg1) { // TODO
+		 * Auto-generated method stub
+		 * 
+		 * } });
+		 */
+		
 		PushManager.getInstance(getApplicationContext()).registerForRegistrationEvents(new PushAppsRegistrationInterface() {
 			
 			@Override
--- Android/Demo/src/com/example/pushappsdemo/MainActivity.java
+++ Android/Demo/src/com/example/pushappsdemo/MainActivity.java
@@ -5,18 +5,28 @@ import com.groboot.pushapps.PushManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		Log.d("DemoApplication", PushManager.getInstance(getApplicationContext()).getDeviceId(getApplicationContext()));
+		Bundle bundle = this.getIntent().getExtras();
+		if (bundle != null) {
+			if (bundle.containsKey(PushManager.NOTIFICATION_MESSAGE_KEY))  {
+				Log.d("DemoApplication", "Message: " + this.getIntent().getExtras().getString(PushManager.NOTIFICATION_MESSAGE_KEY));
+			}
+			if (bundle.containsKey(PushManager.NOTIFICATION_TITLE_KEY))  {
+				Log.d("DemoApplication", "Title: " + this.getIntent().getExtras().getString(PushManager.NOTIFICATION_TITLE_KEY));
+			}
+			if (bundle.containsKey(PushManager.NOTIFICATION_LINK_KEY))  {
+				Log.d("DemoApplication", "Link: " + this.getIntent().getExtras().getString(PushManager.NOTIFICATION_LINK_KEY));
+			}
+			if (bundle.containsKey(PushManager.NOTIFICATION_SOUND_KEY))  {
+				Log.d("DemoApplication", "Sound: " + this.getIntent().getExtras().getString(PushManager.NOTIFICATION_SOUND_KEY));
+			}
+		}
 	}
-
 
 }
